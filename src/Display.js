d6f5a25416d539fbb9d151e6b864fc68cb48bd0c
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2022-10-17 14:14:22 +0900

    fade in の効果をつけた

--- src/Display.js
+++ src/Display.js
@@ -2,16 +2,50 @@ import React, {useEffect, useState} from "react"
 import { Storage } from 'aws-amplify'
 import { useInterval } from './hooks/useInterval'
 import { fetchImages } from './utils'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+  }
+`;
 
 const StyledImage = styled.img`
-  width: "500"
+ background: #f3e2b3;
+ margin-right: calc(50% - 50vw);
+ margin-left: calc(50% - 50vw);
+ animation: ${fadeIn} 5s ease-in-out;
 `
+const Fade = styled.div`
+  display: inline-block;
+  visibility: ${props => props.out ? 'hidden' : 'visible'};
+  animation: ${props => props.out ? fadeOut : fadeIn} 1s linear;
+  transition: visibility 1s linear;
+`;
+
+const FadeIn = styled.div`
+  animation: ${fadeIn} .5s ease-in-out;
+`;
 
 const DisplayPage = () => {
 
   const [index, setIndex] = useState(0)
   const [images, setImages] = useState([])
+  const [visible, setVisible] = useState(true)
 
   useEffect(() => {
     fetchImages().then(images => {
@@ -33,11 +67,10 @@ const DisplayPage = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       {
-         <StyledImage
-            src={images[index]}
-            key={images[index]}
-            style={{width: 500}}
-          />
+           <StyledImage
+              src={images[index]}
+              key={images[index]}
+            />
       }
     </div>
   )
